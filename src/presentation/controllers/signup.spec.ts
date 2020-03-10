eec8ec1f9dac642422da5a867afdb419cd6de694
victorfariasoliveira <jose.victor.oliveira@accenture.com>
2020-03-10 16:06:18 -0300

    test: atualização de teste Deve retornar 400 se o name não for informado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,6 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'qualquer_nome',
         email: 'qualquer_email@email.com',
         password: '112233',
         passwordConfirmation: '112233'
@@ -13,5 +12,22 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Falta de parametro: Nome'))
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Deve retornar 400 se o email não for informado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'qualquer_nome',
+        password: '112233',
+        passwordConfirmation: '112233'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Falta de parametro: Email'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Falta de parametro: Nome')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Falta de parametro: Nome')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Falta de parametro: Email')
+      }
     }
   }
 }
