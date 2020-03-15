11c0d06d517f7b60ce56b704f3df154630444f0a
victorfariasoliveira <jose.victor.oliveira@accenture.com>
2020-03-15 20:57:00 -0300

    refactor: movidos import de protocolos para um unico arquivo index

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,5 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
 import { badRequest, serverError } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
