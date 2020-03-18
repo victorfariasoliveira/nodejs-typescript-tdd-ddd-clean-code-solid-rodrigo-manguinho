858004f68ff020c17dcfb7eddbcefe93ac8e8e5a
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-18 19:37:35 -0300

    test: garantido que o EmailValidatorAdapter retorne true se o validador retornar true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,21 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({ isEmail():boolean {return true}}))
 
 describe('EmailValidator Adapter', () => {
   test('Caso seja retornado falso o validador deve retornar falso', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
+  })
+})
+
+describe('EmailValidator Adapter', () => {
+  test('Caso seja retornado verdadeiro o validador deve retornar verdadeiro', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@gmail.com')
+    expect(isValid).toBe(true)
   })
 })
