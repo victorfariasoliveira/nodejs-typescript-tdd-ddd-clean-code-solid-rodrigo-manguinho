a2620afd46e9acf4be4aa88b1f3b697876a9522f
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-22 18:53:02 -0300

    refactor: limpa o banco entre os testes

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -4,6 +4,10 @@ import { AccountMongoRepository } from './account'
 describe('Account Mongo Repository', () => {
   beforeAll(async () => { await MongoHelper.connect(process.env.MONGO_URL) })
   afterAll(async () => { await MongoHelper.disconnect() })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
