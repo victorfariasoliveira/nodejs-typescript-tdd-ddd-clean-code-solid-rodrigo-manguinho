0bbc91c536c0cbd4474f91d1070b800ef8042809
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-22 18:08:32 -0300

    refactor: renomeado arquivo

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { BcryptAdapter } from './bcrypt-adaper'
+import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
