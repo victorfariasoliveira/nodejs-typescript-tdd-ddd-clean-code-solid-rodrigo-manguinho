545f915f2e69d738771659aa54d6e56ef58fd293
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-22 17:19:26 -0300

    refactor: adição dos termos do test em português

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -16,20 +16,20 @@ describe('Bcrypt Adapter', () => {
   test('Deve chamar o bcrypt com valores corretos', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    await sut.encrypt('qualquer_valor')
+    expect(hashSpy).toHaveBeenCalledWith('qualquer_valor', salt)
   })
 
   test('Deve retornar o hash caso dê sucesso', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
+    const hash = await sut.encrypt('qualquer_valor')
     expect(hash).toBe('hash')
   })
 
   test('Deve retornar a excessão para quem o invocou', async () => {
     const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise = sut.encrypt('any_value')
+    const promise = sut.encrypt('qualquer_valor')
     await expect(promise).rejects.toThrow()
   })
 })
