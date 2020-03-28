f4f4eee9b75a0a9352ee809b63139893b327d5ef
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-28 12:22:43 -0300

    fix: adição do await em métodos assincronos

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -5,7 +5,7 @@ describe('Account Mongo Repository', () => {
   beforeAll(async () => { await MongoHelper.connect(process.env.MONGO_URL) })
   afterAll(async () => { await MongoHelper.disconnect() })
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
---
+++ src/main/decorators/log.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  constructor (private readonly controller: Controller) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      // alguma coisa
+    }
+    return httpResponse
+  }
+}
