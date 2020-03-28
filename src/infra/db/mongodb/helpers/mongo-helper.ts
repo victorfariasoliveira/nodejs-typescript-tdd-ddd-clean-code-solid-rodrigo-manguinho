4f3af8691865d1e95fd225bd41e83e99f725f439
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-03-27 21:20:11 -0300

    feat: adicioando reconexão se o cliente mongodb cair

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,8 +2,10 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(
       uri,
       {
@@ -15,9 +17,14 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client || this.client.isConnected()) {
+      await this.connect(this.uri)
+    }
+
     return this.client.db().collection(name)
   },
 
