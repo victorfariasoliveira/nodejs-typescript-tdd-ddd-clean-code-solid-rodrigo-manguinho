13071f48ead9d5a427cf813f4724bb45db402e78
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-04-05 16:20:52 -0300

    refactor: adição de stacktrace de erro ao serverError

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack: string) {
     super('Error inesperado no servidor')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
